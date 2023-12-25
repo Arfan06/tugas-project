d6615a17e9f8067309677a80a9a201ed77817a0d
Arfan06 <90374710+Arfan06@users.noreply.github.com>
2023-12-26 04:29:22 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -61,7 +61,7 @@ max_date = all_df["order_delivered_customer_date"].max()
  
 with st.sidebar:
     # Menambahkan logo perusahaan
-    st.image("https://github.com/dex-guru/assets/blob/main/chains/43114/chain_43114.svg")
+    st.image(https://github.com/davidbanthony/mega-city-assets/blob/main/Unofficial-Logos/Logo-Chain-Runners.png")
     
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
