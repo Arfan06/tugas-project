8c3bb6047553bdf2e8b8aa812fdb9e5efdb5425f
Arfan06 <90374710+Arfan06@users.noreply.github.com>
2023-12-26 04:22:39 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -61,7 +61,7 @@ max_date = all_df["order_delivered_customer_date"].max()
  
 with st.sidebar:
     # Menambahkan logo perusahaan
-    st.image("img/fcc_secondary_small.svg")
+    st.image("https://github.com/WalletConnect/walletconnect-assets/blob/master/Logo/Blue%20(Default)/Logo.png")
     
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
